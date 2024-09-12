8c1c129838d6275788da9da6009c68d0a5e9ca83
Rayen Ben Hassine <rayenbenhassindjo@gmail.com>
2024-09-12 10:59:46 +0100

    edit start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Define the path to the database file
-DB_PATH="./instance/app.db"
+# Define the path to the instance directory and database file
+INSTANCE_DIR="./instance"
+DB_PATH="$INSTANCE_DIR/app.db"
+
+# Check if the instance directory exists, if not, create it
+if [ ! -d "$INSTANCE_DIR" ]; then
+  echo "Creating instance directory..."
+  mkdir -p "$INSTANCE_DIR"
+fi
 
 # Check if the database file exists
 if [ ! -f "$DB_PATH" ]; then
